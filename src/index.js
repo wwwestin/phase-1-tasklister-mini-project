80fa28ff2a9b0c62ad26b043a254cd2fdfd686b1
Westin Humble <westin.h@gmail.com>
2022-01-18 11:01:15 -0500

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const newTaskDescription = document.getElementById("new-task-description");
+const theList = document.getElementById("tasks");
+
+const createNewTask = event => { 
+  event.preventDefault(); 
+  const newTask = document.createElement('li');
+
+  newTask.innerText = newTaskDescription.value;
+  createButton(newTask);
+  theList.appendChild(newTask);
+
+  event.target.reset();
+};
+
+const createButton = task => {
+   const btn = document.createElement('button');
+    btn.innerText = "X";
+
+    task.appendChild(btn);
+    btn.addEventListener('click',  (e) => deleteTask(e));
+}
+
+const deleteTask = (e) => {
+  const selectedTask = e.target.parentElement;
+  theList.removeChild(selectedTask);
+}
+
+document.querySelector('#create-task-form').addEventListener("submit"
+, (e) => createNewTask(e))
+
+
